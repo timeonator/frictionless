69a7d32c28691127c368d562a58c6495d1e06d28
timeonator <timeonator@gmail.com>
2021-08-22 18:07:53 -0700

    prettier- no other change

--- src/components/resources.tsx
+++ src/components/resources.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
 import MaterialTable from 'material-table';
 
-
-interface Heading { 
-    field: string;
-    title : string;
+interface Heading {
+  field: string;
+  title: string;
 }
 interface Data {
-    name: string;
-    path: string;
-    title: string;
+  name: string;
+  path: string;
+  title: string;
 }
 
-
-const columns : Array<Heading> = [
+const columns: Array<Heading> = [
   { field: 'name', title: 'Name' },
   { field: 'path', title: 'Path' },
   { field: 'title', title: 'Title' },
 ];
 
-const data : Array<Data> = [
+const data: Array<Data> = [
   {
     name: 'solar-system',
     path: 'http://example.com/solar-system.csv',
